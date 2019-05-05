71b6bf88c751916d88a4eedf7cdec2dd10cc1052
Adrian Suter <mail@adriansuter.ch>
2019-05-05 18:46:39 +0200

    Add basic features

---
+++ tests/test.php
@@ -0,0 +1,10 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://localhost/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$output = curl_exec($ch);
+curl_close($ch);
+
+echo $output;
